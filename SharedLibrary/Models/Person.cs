406ad464c7d446cae4788453c687bf0968a298e9
Nataliya Lisjo <natlisjo@gmail.com>
2020-09-22 12:09:28 +0200

    WorkerServiceExemle

---
+++ SharedLibrary/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Models
+
+{
+	public class Person
+	{
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+
+	}
+
+}
--- SharedLibrary/Models/TemperatureModel.cs
+++ SharedLibrary/Models/TemperatureModel.cs
@@ -5,6 +5,9 @@ namespace SharedLibrary.Models
     public class TemperatureModel //  måste public för access från andra project
     {
         public double Temperature { get; set; }
+
         public double Humidity { get; set; } //влажность
+
+        public DateTime Timestamp { get; set; }
     }
 }
